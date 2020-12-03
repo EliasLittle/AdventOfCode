c6c82d98e0832f5c260cb6bb63db59cc5af15708
Elias Little <eliasjlittle@gmail.com>
2020-12-03 01:38:31 -0600

    Day 3 solutions

--- AdventofCode.jl
+++ AdventofCode.jl
@@ -434,6 +434,127 @@ end
 # ╔═╡ 14c21e00-34c6-11eb-0b8f-511b8ec911bb
 solution4("Day2.txt")
 
+# ╔═╡ 24b0a9a4-352e-11eb-3c8c-e5bb60df6bbd
+md"""
+"""
+
+# ╔═╡ 2dd8df38-352e-11eb-03d4-555ad8ef47e5
+md"""
+---
+### Question 5
+
+With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
+
+Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
+```
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+```
+These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
+```
+..##.........##.........##.........##.........##.........##.......  ---> \
+#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..\
+.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.\
+..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#\
+.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........#.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...##....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+```
+You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
+
+The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
+
+From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
+
+The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
+```
+..##.........##.........##.........##.........##.........##.......  --->
+#..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........X.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...#X....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+```
+In this example, traversing the map using this slope would cause you to encounter 7 trees.
+
+Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
+"""
+
+# ╔═╡ 98c75738-352f-11eb-28a9-01831e99f127
+begin
+	function solution5(input,x_incr,y_incr)
+		tree_count = open(input) do file
+			map, tree_count = [],0
+			for line in eachline(file)
+				push!(map, collect(line))
+		    end
+			
+			x,y = 1,1
+			while y ≤ 323
+				if map[y][x] == '#'
+					tree_count += 1
+				end
+				x += x_incr
+				if x > 31
+					x -= 31
+				end
+				y += y_incr
+			end
+			tree_count
+		end
+	end
+end
+
+# ╔═╡ 04a1c0a0-3536-11eb-021a-99137b91e87e
+solution5("Day3.txt",3,1)
+
+# ╔═╡ 90090656-3538-11eb-3037-1bb261555d15
+md"""
+### Question 6
+
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+Right 1, down 1. \
+Right 3, down 1. (This is the slope you already checked.) \
+Right 5, down 1. \
+Right 7, down 1. \
+Right 1, down 2. \
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+"""
+
+# ╔═╡ d7f7f5d0-3538-11eb-3900-27db227e4f30
+begin
+	function solution6(slopes)
+		sols = [solution5("Day3.txt",x,y) for (x,y) in slopes]
+		prod(sols)
+	end
+end
+
+# ╔═╡ b00ac57e-3539-11eb-1805-533701bcde60
+solution6([(1,1),(3,1),(5,1),(7,1),(1,2)])
+
 # ╔═╡ Cell order:
 # ╟─cd079d24-33e9-11eb-2657-b5d8b026582a
 # ╠═372ffe08-33ea-11eb-2124-85a9b6123282
@@ -456,3 +577,10 @@ solution4("Day2.txt")
 # ╟─3d595e90-34c5-11eb-1f2d-1b7a04e6b578
 # ╠═ad7bf25c-34c5-11eb-1f69-59ac12cd266f
 # ╠═14c21e00-34c6-11eb-0b8f-511b8ec911bb
+# ╟─24b0a9a4-352e-11eb-3c8c-e5bb60df6bbd
+# ╟─2dd8df38-352e-11eb-03d4-555ad8ef47e5
+# ╠═98c75738-352f-11eb-28a9-01831e99f127
+# ╠═04a1c0a0-3536-11eb-021a-99137b91e87e
+# ╟─90090656-3538-11eb-3037-1bb261555d15
+# ╠═d7f7f5d0-3538-11eb-3900-27db227e4f30
+# ╠═b00ac57e-3539-11eb-1805-533701bcde60
